8929873ec2b0c67b74a7f626c9866f52ee2b0be6
Bradley Milton <bmilton2412@gmail.com>
2023-04-25 11:00:41 -0700

    add ride, biker, and bike_club class

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,3 +1,25 @@
 class Biker
-  
+  attr_reader :name, :max_distance, :rides, :acceptable_terrain
+
+  def initialize(name, max_distance)
+    @name = name
+    @max_distance = max_distance
+    @rides = {}
+    @acceptable_terrain = []
+  end
+
+  def learn_terrain!(terrain)
+    @acceptable_terrain << terrain
+  end
+
+  def log_ride(ride, times)
+    if ride.total_distance < @max_distance && @acceptable_terrain.include?(ride.terrain)
+      @rides[ride] ||= []
+      @rides[ride] << times
+    end
+  end
+
+  def personal_record(ride)
+    @rides[ride]&.min
+  end
 end
--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,18 @@
 class Ride
+  attr_reader :name, :distance, :loop, :terrain
 
+  def initialize(attributes)
+    @name = attributes[:name]
+    @distance = attributes[:distance]
+    @loop = attributes[:loop]
+    @terrain = attributes[:terrain]
+  end
+
+  def loop?
+    @loop
+  end
+
+  def total_distance
+    @loop ? @distance : @distance * 2
+  end
 end
